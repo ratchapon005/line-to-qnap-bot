8d82391a43c28c2b78ad9f95b85bedd42845477b
ratchapon005 <Ratchaponmat005@gmail.com>
2025-06-25 16:10:17 +0700

    app.py

--- app.py
+++ app.py
@@ -52,4 +52,6 @@ def handle_file_message(event):
     os.remove(filename)
 
 if __name__ == "__main__":
-    app.run()
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
+
